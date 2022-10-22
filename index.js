020655c561fde00c1f504465701b7c96547a526b
NeyeReQ <94548227+NeyeReQ@users.noreply.github.com>
2022-10-22 17:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const discord = require("discord.js")
+
+const TOKEN = "MY TOKEN"
 const client = new discord.Client({
     intents: [
         "Guilds",
@@ -6,12 +8,10 @@ const client = new discord.Client({
         "MessageContent" 
     ]
 })
-<<<<<<< HEAD
 
 bot.on("Ready", function (){
     console.log(`${bot.user.username} Is Online!`);
 });
 
 bot.login(process.env.token);
-=======
->>>>>>> 02fae9b6d79b01c1c69d10a25c0d6b88bac21e49
+client.login(TOKEN)
